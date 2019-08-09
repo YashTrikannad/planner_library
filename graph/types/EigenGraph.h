ac89a7f84ac4d59a0a332e85be0c8b0d86cf575e
YashTrikannad <yash.trikannad@gmail.com>
2019-08-08 23:22:34 -0400

    Add basic functionality to Eigen Graph Interface

--- graph/types/EigenGraph.h
+++ graph/types/EigenGraph.h
@@ -4,7 +4,9 @@
 
 #pragma once
 
+#include <vector>
 #include "../pfl_graph.h"
+#include "../../common/data_types.h"
 
 
 namespace pfl::graph
@@ -16,8 +18,8 @@ class eigen_graph
 {
 public:
     using graph_type = Graph;
-    using node_type = typename Graph::Scalar;
-
+    using data_type = typename Graph::Scalar;
+    using node_type = pfl::common::NodeIndex2d;
 
     eigen_graph(const graph_type &graph): graph_(graph)
     {
@@ -27,13 +29,33 @@ public:
     /// Func is applied to each adjacent node of the current node
     /// @tparam Func
     template<typename Func>
-    void for_each_adjacent_node(const node_type &node, Func &&func) const = delete;
+    void for_each_adjacent_node(const node_type &node, Func &&func) const
+    {
+        std::vector<pfl::common::NodeIndex2d> neighbors = {{node.row_index_ , node.row_index_ },
+                                                           {node.row_index_ , node.row_index_ + 1},
+                                                           {node.row_index_ + 1, node.row_index_ },
+                                                           {node.row_index_ + 1, node.row_index_ + 1}};
+        for(const auto& neighbor: neighbors)
+        {
+            func(neighbor);
+        }
+    }
 
 
     /// Func is applied to all the nodes in the graph
     /// @tparam Func
     template<typename Func>
-    void for_each_node() const = delete;
+    void for_each_node(Func &&func) const
+    {
+        for(size_t col=0; col < graph_.cols(); col++)
+        {
+            for(size_t row=0; row < graph_.rows(); row++)
+            {
+                func(pfl::common::NodeIndex2d{row, col});
+            }
+        }
+
+    }
 
 
 private:
